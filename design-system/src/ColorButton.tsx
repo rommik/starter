4b6c4c53f3bd7f9e5457c242d3518f2b43d2b310
Roman Mikhailov <roman.mikhailov@barco.com>
2020-08-25 23:11:08 -0400

    configure colorButton to be tsx

--- design-system/src/ColorButton.tsx
+++ design-system/src/ColorButton.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-export interface IProps {
+export interface ColorButtonProps {
     color: string
     onClick?: (color: string) => void
 }
-export default (props: IProps) => {
+export const ColorButton = (props: ColorButtonProps) => {
     const {color, onClick} = props;
     return <button style={{color}} onClick={() => onClick && onClick(color)}>Color Button</button>
-}
+};
